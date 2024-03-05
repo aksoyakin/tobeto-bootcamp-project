6b71c2adbeb4eb48d2edb0c9500f1f64a738c4eb
AKIN AKSOY <akinaksoyiletisim@gmail.com>
2024-03-05 09:24:15 +0300

    BaseEntity created.

--- src/main/java/dev/akinaksoy/tobetobootcampproject/core/baseentity/BaseEntity.java
+++ src/main/java/dev/akinaksoy/tobetobootcampproject/core/baseentity/BaseEntity.java
@@ -1,4 +1,30 @@
 package dev.akinaksoy.tobetobootcampproject.core.baseentity;
 
-public class BaseEntity {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+public class BaseEntity<T> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private T id;
+
+    @Column(name = "createdDate")
+    private LocalDateTime createdDate;
+
+    @Column(name = "updatedDate")
+    private LocalDateTime updatedDate;
+
+    @Column(name = "deletedDate")
+    private LocalDateTime deletedDate;
+
 }
