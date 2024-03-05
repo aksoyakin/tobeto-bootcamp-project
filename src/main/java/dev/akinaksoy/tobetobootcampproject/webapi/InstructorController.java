7f8d458ef6dd491152466537bf03336e8db33dbc
AKIN AKSOY <akinaksoyiletisim@gmail.com>
2024-03-05 22:40:15 +0300

    refactor

--- src/main/java/dev/akinaksoy/tobetobootcampproject/webapi/InstructorController.java
+++ src/main/java/dev/akinaksoy/tobetobootcampproject/webapi/InstructorController.java
@@ -2,8 +2,6 @@ package dev.akinaksoy.tobetobootcampproject.webapi;
 
 import dev.akinaksoy.tobetobootcampproject.business.abstracts.InstructorService;
 import dev.akinaksoy.tobetobootcampproject.business.request.create.CreateInstructorRequest;
-import dev.akinaksoy.tobetobootcampproject.business.response.create.CreateInstructorResponse;
-import dev.akinaksoy.tobetobootcampproject.entities.Instructor;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/instructors")
