fe1c386c1ef533ab421d72f0fb081c8631a397ec
AKIN AKSOY <akinaksoyiletisim@gmail.com>
2024-03-05 12:31:41 +0300

    create

---
+++ src/main/java/dev/akinaksoy/tobetobootcampproject/business/request/create/CreateApplicationStateRequest.java
@@ -0,0 +1,15 @@
+package dev.akinaksoy.tobetobootcampproject.business.request.create;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateApplicationStateRequest {
+
+    @NotEmpty(message = "Name must not be empty!")
+    private String name;
+}
---
+++ src/main/java/dev/akinaksoy/tobetobootcampproject/business/response/create/CreateApplicationStateResponse.java
@@ -0,0 +1,15 @@
+package dev.akinaksoy.tobetobootcampproject.business.response.create;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateApplicationStateResponse {
+
+    private int id;
+    private String name;
+
+}
